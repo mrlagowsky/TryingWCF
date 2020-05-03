f71738d4047299726223080c95ad903f2da4b8d1
Kacper Lagowski <kac.lagowski@gmail.com>
2020-05-03 10:59:16 +0200

    First commit

---
+++ ServiceClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DayLogServiceReference.DayLogServiceClient proxy = new DayLogServiceReference.DayLogServiceClient();
+            string message = proxy.GetData(101);
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
